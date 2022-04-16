f980f895466fbb4c5e9675699709f7f433f5bfdb
Hyunsik Kim <devconf5296@gmail.com>
2022-04-16 15:01:44 +0900

    Chore:Initialize Spring-boot and Connect H2 DB

---
+++ src/main/java/com/wowtown/wowtownbackend/WowTownBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wowtown.wowtownbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowTownBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowTownBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wowtown/wowtownbackend/WowTownBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wowtown.wowtownbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowTownBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
