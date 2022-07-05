7d198fc4d96777bf258c5d908ceed4880eda26d2
Hyunsik Kim <devconf5296@gmail.com>
2022-07-05 17:23:26 +0900

    Refactor Avatar API

--- src/main/java/com/wowtown/wowtownbackend/avatar/infra/JpaAvatarRepository.java
+++ src/main/java/com/wowtown/wowtownbackend/avatar/infra/JpaAvatarRepository.java
@@ -8,20 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public interface JpaAvatarRepository extends JpaRepository<Avatar, Long>, AvatarRepository {
-  @Query("select av from Avatar as av where av.channel.id =:channelId and av.nickName =:nickName")
-  Optional<Avatar> findAvatarWithChannelIdAndNickName(
-      @Param("channelId") Long channelId, @Param("nickName") String nickName);
-
   @Query("select av from Avatar as av where av.channel.id =:channelId and av.user.id =:userId")
   Optional<Avatar> findAvatarWithChannelIdAndUserId(
       @Param("channelId") Long channelId, @Param("userId") Long userId);
 
-  /*@Query("select av from Avatar as av where av.user.id =:userId")
-  Set<Avatar> findAvatarWithUserId(
-          @Param("userId") Long userId);*/
-
+  @Query(
+      "select av from Avatar as av where av.channel.id =:channelId and av.user.id =:userId and av.nickName =:nickName")
+  Optional<Avatar> findAvatarWithChannelIdAndUserIdAndNickName(
+      @Param("channelId") Long channelId,
+      @Param("userId") Long userId,
+      @Param("nickName") String nickName);
 }
