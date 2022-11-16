c121912ffcc69ce0c851df29f5bde44c5d0abb92
Hyunsik Kim <devconf5296@gmail.com>
2022-11-16 20:26:17 +0900

    Remove websocketSubscribeEvent

--- src/main/java/com/wowtown/wowtownbackend/common/event/WebsocketSubscribeEvent.java
+++ src/main/java/com/wowtown/wowtownbackend/common/event/WebsocketSubscribeEvent.java
@@ -1,17 +1,17 @@
-package com.wowtown.wowtownbackend.common.event;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.web.socket.messaging.SessionSubscribeEvent;
-
-@Configuration
-@RequiredArgsConstructor
-public class WebsocketSubscribeEvent {
-  @EventListener
-  public void onSubscribeEvent(SessionSubscribeEvent event) {
-    SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
-    System.out.println(headerAccessor.getSessionId());
-  }
-}
+// package com.wowtown.wowtownbackend.common.event;
+//
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.event.EventListener;
+// import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+// import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+//
+// @Configuration
+// @RequiredArgsConstructor
+// public class WebsocketSubscribeEvent {
+//  @EventListener
+//  public void onSubscribeEvent(SessionSubscribeEvent event) {
+//    SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
+//    System.out.println(headerAccessor.getSessionId());
+//  }
+// }
