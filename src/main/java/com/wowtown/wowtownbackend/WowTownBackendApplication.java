95abc7c0dd8feaa4e94ed98b4d124f25efc2dc57
Hyunsik Kim <devconf5296@gmail.com>
2022-11-16 17:38:59 +0900

    Add Memory Info logging

--- src/main/java/com/wowtown/wowtownbackend/WowTownBackendApplication.java
+++ src/main/java/com/wowtown/wowtownbackend/WowTownBackendApplication.java
@@ -1,12 +1,35 @@
 package com.wowtown.wowtownbackend;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.NumberFormat;
+
+@Log4j2
 @SpringBootApplication
 public class WowTownBackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WowTownBackendApplication.class, args);
+    Runtime runtime = Runtime.getRuntime();
+
+    final NumberFormat format = NumberFormat.getInstance();
+
+    final long maxMemory = runtime.maxMemory();
+    final long allocatedMemory = runtime.totalMemory();
+    final long freeMemory = runtime.freeMemory();
+    final long mb = 1024 * 1024;
+    final String mega = " MB";
+
+    log.info("========================== Memory Info ==========================");
+    log.info("Free memory: " + format.format(freeMemory / mb) + mega);
+    log.info("Allocated memory: " + format.format(allocatedMemory / mb) + mega);
+    log.info("Max memory: " + format.format(maxMemory / mb) + mega);
+    log.info(
+        "Total free memory: "
+            + format.format((freeMemory + (maxMemory - allocatedMemory)) / mb)
+            + mega);
+    log.info("=================================================================\n");
   }
 }
